3bb4d47c58a972486e79c0519cec730f5040cbb0
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-04-19 02:31:47 -0300

    Update to version 1.2.1

--- conanfile.py
+++ conanfile.py
@@ -5,9 +5,13 @@ import os
 
 # TODO: Add an option to enable Xeon Phi support. See https://github.com/STEllAR-GROUP/hpx#intelr-xeonphi
 
+
+# TODO: Remove examples
+# TODO: Remove documentation
+
 class HpxConan(ConanFile):
     name = "hpx"
-    version = "1.1.0"
+    version = "1.2.1"
     license = "Boost Software License, Version 1.0"
     url = "https://github.com/darcamo/conan-hpx"
     description = "HPX (High Performance ParalleX) is a general purpose C++ runtime system for parallel and distributed applications of any scale."
@@ -15,13 +19,13 @@ class HpxConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
-    requires = "boost/1.66.0@conan/stable"  #, "hwloc/1.11.1@lasote/stable"
+    requires = "boost/1.70.0@conan/stable", "hwloc/1.11.12@darcamo/stable"
 
     def system_requirements(self):
         if tools.os_info.linux_distro == "arch":
-            package_names = ["gperftools", "hwloc"]
+            package_names = ["gperftools"]
         elif tools.os_info.linux_distro == "ubuntu":
-            package_names = ["libgoogle-perftools-dev", "libhwloc-dev"]
+            package_names = ["libgoogle-perftools-dev"]
         else:
             package_names = []
 
@@ -39,6 +43,7 @@ conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["HPX_WITH_EXAMPLES"] = False
         os.mkdir("build")
         shutil.move("conanbuildinfo.cmake", "build/")
         cmake.configure(source_folder="sources", build_folder="build")
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,27 +1,21 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+///////////////////////////////////////////////////////////////////////////////
+// The purpose of this example is to execute a HPX-thread printing
+// "Hello World!" once. That's all.
+
+//[simplest_hello_world_1_getting_started
 // Including 'hpx/hpx_main.hpp' instead of the usual 'hpx/hpx_init.hpp' enables
 // to use the plain C-main below as the direct main HPX entry point.
 #include <hpx/hpx_main.hpp>
-#include <hpx/include/actions.hpp>
-#include <hpx/include/components.hpp>
 #include <hpx/include/iostreams.hpp>
-#include <hpx/include/lcos.hpp>
-#include <hpx/include/util.hpp>
 
-#include <cstddef>
-#include <list>
-#include <mutex>
-#include <set>
-#include <vector>
-
-int main()
-{
-    // Get a list of all available localities.
-    std::vector<hpx::naming::id_type> localities =
-        hpx::find_all_localities();
-    std::size_t const os_threads = hpx::get_os_thread_count();
-
-    std::cout << "Running hpx in " << localities.size() << " locality(ies) and current locality has " << os_threads << " threads." << std::endl;
-
-    return 0;
+int main() {
+  // Say hello to the world!
+  hpx::cout << "Hello World!\n" << hpx::flush;
+  return 0;
 }
 //]
